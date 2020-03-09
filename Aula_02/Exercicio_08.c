a174da4924d6aa8fb049186126ae8b333c9ea092
ivovieirajunior <ivo.vieira.1997@hotmail.com>
2020-03-09 11:36:04 -0300

    Atividade

--- Aula_02/Exercicio_08.c
+++ Aula_02/Exercicio_08.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
+
+/* 1)Tendo um array de inteiros (valores) com 20000 posi��es (inicializado automaticamente onde a
+		posi��o corresponde a valor), pretende-se calcular resultado[i] = valores[i] * 5 + 55. 
+		
+		Altere o c�lculo do exerc�cio anterior para  resultado[i] = valores[i] * rand() ate 5 + rand() 
+		at� 1000. */
+
 
 int resultados1[10000];
 //int resultados2[10000];
@@ -9,8 +17,10 @@ void * HelloThread1(void * argumento){ //função que retorna um ponteiro "gené
     pthread_t NumThread; //declara NumThread como um thread
     NumThread = pthread_self(); //NumThread recebe o id do thread onde esta função está rodando
     int i = 0;
-    for (i = 0; i < 5000; i++){
-       resultados1[i] = i *5 + 55;
+    for (i = 0; i < 5; i++){
+    	int num = (rand() % 5);
+       resultados1[i] = i * num + 55;
+       //printf("rand(): %d \n" , num);
     }
     return NumThread; //equivalente a chamar pthread_exit(NumThread);
 }
@@ -31,6 +41,7 @@ void * HelloThread3(void * argumento){ //função que retorna um ponteiro "gené
     int i = 0;
     for (i = 10000; i < 15000; i++){
        resultados1[i] = i *5 + 55;
+       
     }
     return NumThread; //equivalente a chamar pthread_exit(NumThread);
 }
@@ -44,13 +55,11 @@ void * HelloThread4(void * argumento){ //função que retorna um ponteiro "gené
     }
     return NumThread; //equivalente a chamar pthread_exit(NumThread);
 }
-
-
-int main(){
-    clock_t begin = clock();
-
-    pthread_t thread1 , thread2, thread3, thread4;
-    int flag1 = 1;
+int main(int argc, char *argv[]) {
+	clock_t begin = clock();
+	
+    pthread_t thread1 , thread2 ,thread3 ,thread4 ;
+    int flag1 = 1 ,flag2= 0;
     int n1 = 1;
 
 
@@ -59,39 +68,43 @@ int main(){
         printf("\nLinha logo após a primeira chamada de pthread_create.");
     if (flag1 != 0) //se retornou um valor diferente de 0 é porque deu erro na criação do thread
         printf("Erro na criação da thread 1!\n");
-
-    flag1 = pthread_create(&thread2, NULL, HelloThread2, &n1); //cria o thread com identificador thread que vai executar a função HelloThread
+	
+    flag2 = pthread_create(&thread2, NULL, HelloThread2, &n1); //cria o thread com identificador thread que vai executar a função HelloThread
         printf("\nLinha logo após a segunda chamada de pthread_create.");
-    if (flag1 != 0) //se retornou um valor diferente de 0 é porque deu erro na criação do thread
+    if (flag2 != 0) //se retornou um valor diferente de 0 é porque deu erro na criação do thread
         printf("Erro na criação da thread 2!\n");
-    flag1 = pthread_create(&thread3, NULL, HelloThread3, &n1); //cria o thread com identificador thread que vai executar a função HelloThread
+    /*flag1 = pthread_create(&thread3, NULL, HelloThread3, &n1); //cria o thread com identificador thread que vai executar a função HelloThread
         printf("\nLinha logo após a terceira chamada de pthread_create.");
     if (flag1 != 0) //se retornou um valor diferente de 0 é porque deu erro na criação do thread
         printf("Erro na criação da thread 3!\n");
-
     flag1 = pthread_create(&thread4, NULL, HelloThread4, &n1); //cria o thread com identificador thread que vai executar a função HelloThread
         printf("\nLinha logo após a quarta chamada de pthread_create.");
     if (flag1 != 0) //se retornou um valor diferente de 0 é porque deu erro na criação do thread
         printf("Erro na criação da thread 4!\n");
-
+	*/
 
     pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
-    pthread_join(thread3, NULL);
-    pthread_join(thread4, NULL);
-
+    //pthread_join(thread2, NULL);
+    //pthread_join(thread3, NULL);
+    //pthread_join(thread4, NULL);
+ 	
     clock_t end = clock();
 
 
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Tempo percorrido foi de: %fs.\n" , time_spent);
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC ;
+     printf("\nTempo percorrido foi de: %fs  -  %fs\n" , (double)end , (double)begin);
+    printf("\nTempo percorrido foi de: %fs \n" , time_spent);
+    
     int i;
-    for ( i = 0; i < 3; i++)
+    
+    for (i = 0; i < 3; i++)
     {
         printf("Elemento [%d]: %d .\n", i ,resultados1[i]);
 
     }
     
-
-    return 0;
+    if(pthread_join(thread1, NULL)){
+    		return 0;
+	}
+	
 }
